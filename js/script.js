7a1b13a6f3e9dcbc6a48ae02e8c4e28d6b3e08ee
Artur <krivenko.artur07@gmail.com>
2025-11-02 16:34:30 +0100

    Fix bags, add all styles for modal and mobile menus and fix link in mobile menu for click

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 const modal = document.querySelector('.backdrop');
-const modalBtnOpen = document.querySelector('.modal__button-open');
+const modalBtnsOpen = document.querySelectorAll('.modal__button-open'); // все кнопки
 const modalBtnClose = document.querySelector('.modal__button-close');
 
 const toggleModal = () => modal.classList.toggle('is-hidden');
 
-modalBtnOpen.addEventListener('click', toggleModal);
+// добавляем обработчик ко всем кнопкам открытия
+modalBtnsOpen.forEach(btn => {
+  btn.addEventListener('click', toggleModal);
+});
+
+// обработчик закрытия
 modalBtnClose.addEventListener('click', toggleModal);
